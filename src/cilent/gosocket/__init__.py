3e5871f2fa2badfc4b8aa9020ab727ef4f0044d3
Ayala Kaguya <papermo.io@gmail.com>
2021-05-16 12:54:14 +0800

    优化注释

--- src/cilent/gosocket/__init__.py
+++ src/cilent/gosocket/__init__.py
@@ -5,5 +5,11 @@ __author__ = 'AyalaKaguya <ayalakaguya@outlook.com>'
 __all__ = [go]
 
 
-def go(serv, port):
+def go(serv: str, port: int) -> mGo.go:
+    """
+    创建一个goSocket客户端实例
+
+    serv: 服务器地址
+    port: 服务器端口
+    """
     return mGo.go(serv, port)
